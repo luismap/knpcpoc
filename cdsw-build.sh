77297eb2090200042438625e98494b3ad3f07b37
luis.map <luismap2508@gmail.com>
2024-10-03 09:00:25 +0200

    [build] adding query cdsw build info

--- cdsw-build.sh
+++ cdsw-build.sh
@@ -30,6 +30,10 @@ elif [[ $srvc_type = "llm" ]]
 then
     echo "installing for $srvc_type"
     pip3 install -U -r requirements_hf.txt
+elif [[ $srvc_type = "qexpansion" ]]
+then
+    echo "installing for $srvc_type"
+    pip3 install -U -r requirements_hf.txt
 
 else
     echo "could not parse/or find env var srvc_type"
