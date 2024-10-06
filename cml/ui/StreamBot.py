b6205772ebe845d4ecabf9d46066dd0fe2bfbc71
luis.map <luismap2508@gmail.com>
2024-10-06 09:07:42 +0200

    [refac] preparing stream bot for generation

--- cml/ui/StreamBot.py
+++ cml/ui/StreamBot.py
@@ -1,30 +1,52 @@
 from typing import List
+import torch
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     TextIteratorStreamer,
+    BitsAndBytesConfig,
 )
+import os
+from huggingface_hub import login
 
-# from huggingface_hub import login as lg
+login(os.getenv("hf_token"))
+
+from huggingface_hub import login as lg
 from threading import Thread
+
+bnb_4bit_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_compute_dtype=torch.bfloat16,
+)
 
 
 model_id = "google/gemma-2-9b-it"
 model_id_sm = "openai-community/gpt2"
+current_model = model_id_sm
 
-tok = AutoTokenizer.from_pretrained(model_id_sm)
-model = AutoModelForCausalLM.from_pretrained(model_id_sm)
-streamer = TextIteratorStreamer(tok)
+tokenizer = AutoTokenizer.from_pretrained(current_model)
+decoder_kwargs = {"skip_special_tokens": True}
+model = AutoModelForCausalLM.from_pretrained(
+    current_model,
+    torch_dtype=torch.bfloat16,
+    quantization_config=bnb_4bit_config,
+)
+
+streamer = TextIteratorStreamer(tokenizer, decoder_kwargs=decoder_kwargs)
 
 
-def generate_inputs(text: List[str] = ["tell me a joke"]):
-    inputs = tok(text, return_tensors="pt")
+def generate_inputs(chat: List[dict[str, str]] = []):
+    inputs = tokenizer.apply_chat_template(
+        chat, tokenize=True, add_generation_prompt=True, return_tensors="pt"
+    )  # .to("cuda")
     return inputs
 
 
 def get_stream_output(tokenized_input, max_new_tokens: int = 30):
     generation_kwargs = dict(
-        tokenized_input, streamer=streamer, max_new_tokens=max_new_tokens
+        inputs=tokenized_input, streamer=streamer, max_new_tokens=max_new_tokens
     )
     thread = Thread(target=model.generate, kwargs=generation_kwargs)
     thread.start()
