758bad5275bb1b44c546b38e4aa3afe6565d93be
luis.map <luismap2508@gmail.com>
2024-10-07 11:23:39 +0200

    [feat] updating with deepcopy to avoid collisions (tb refactor in future)

--- knpc_ui.py
+++ knpc_ui.py
@@ -1,25 +1,27 @@
+from copy import deepcopy
 import streamlit as st
 from streamlit_chat import message
 from streamlit.components.v1 import html
 from core.utils.ChatHistory import ChatHistory
-# from StreamBot import StreamBot
-# from Reranker import Reranker
+from core.utils.StreamBot import StreamBot
+from core.utils.Reranker import Reranker
 
 
-st.title("KNPC Bot")
+st.title("Knowledge base AI🤖")
+st.logo("resources/img/knpclogo_400x400.jpg", size="large")
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = ChatHistory()
-# if "streambot" not in st.session_state:
-#     st.session_state.streambot = StreamBot()
-# if "reranker" not in st.session_state:
-#     st.session_state.reranker = Reranker()
+if "streambot" not in st.session_state:
+    st.session_state.streambot = StreamBot()
+if "reranker" not in st.session_state:
+    st.session_state.reranker = Reranker()
 if "user_id" not in st.session_state:
     st.session_state.user_id = "default"
 
 user_id = st.sidebar.text_input("👤 username", value=st.session_state.user_id)
-st.session_state.user_name = user_id
+st.session_state.user_id = user_id
 
 # Display chat messages from history on app rerun
 for message in st.session_state.messages.get_history(user_id=user_id):
@@ -59,7 +61,8 @@ def get_current_chat_context():
     return chat_ctx
 
 
-def current_context_from_history(user_id):
+def current_context_from_history():
+    user_id = st.session_state.user_id
     return st.session_state.messages.get_history(user_id=user_id)
 
 
@@ -71,11 +74,10 @@ chat = [
 
 # React to user input
 if prompt := st.chat_input("Ask me something..."):
-    # stbot = st.session_state.streambot
-    # reranker = st.session_state.reranker
+    stbot = st.session_state.streambot
+    reranker = st.session_state.reranker
     # text_context = get_current_chat_context()
-    user_id = st.session_state.user_id
-    text_context = current_context_from_history(user_id)
+    text_context = current_context_from_history()
 
     # Display user message in chat message container
     with st.chat_message("user"):
@@ -88,28 +90,34 @@ if prompt := st.chat_input("Ask me something..."):
                 "top_n": 4,
                 "embeddings": 0,
             }
-            # top_docs = reranker.rerank(reranker_payload)
+            top_docs = reranker.rerank(reranker_payload)
 
     with st.chat_message("assistant"):
-        # context = get_llm_context(top_docs)
-        # footer = get_source_info(top_docs)
-        # prompt_context = f"""using the following context
-        #     ##context
-        #     {context}
-        #     answer this question:
-        #     {prompt}
-        # """
-        # text_context.append({"role": "user", "content": prompt_context})
+        context = get_llm_context(top_docs)
+        footer = get_source_info(top_docs)
+        prompt_context = f"""using the following context answer the question. if the context is too short
+            or does not related to the question, then answer that you user needs to expand query to retrieve more context.
+            ##context
+            {context}
+            ##question:
+            {prompt}
+        """
+        # create new copy with enhanced context
+        llm_context = deepcopy(text_context)
+        llm_context.append({"role": "user", "content": prompt_context})
+        # copy just the prompt to the history
         text_context.append({"role": "user", "content": prompt})
-        # inputs = stbot.generate_inputs(text_context)
+        # text_context.append({"role": "user", "content": prompt})
+        inputs = stbot.generate_inputs(llm_context)
         with st.spinner("asking llm with enhanced context..."):
+            a = st.write_stream(stbot.get_stream_output(inputs, max_new_tokens=10000))
+            st.write(footer)
             # st.write(a)
-            # a = st.write_stream(stbot.get_stream_output(inputs, max_new_tokens=10000))
-            # st.write(footer)
-            st.write(text_context)
-            ai_rsp = {"role": "assistant", "content": "some random content"}
+            # st.write(text_context)
+            ai_rsp = {"role": "assistant", "content": a}
 
     # # Add user message to chat history
     # st.session_state.messages.append({"role": "user", "content": prompt})
     # st.session_state.messages.append({"role": "assistant", "content": a})
+    user_id = st.session_state.user_id
     st.session_state.messages.add_history(user_id, prompt, ai_rsp)
